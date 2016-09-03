7dccbc7ba0290a093763ec442d84192a6b3154b6
nick wong <chaoqingwang@foxmail.com>
2016-09-03 12:19:55 -0700

    bug fix;

--- R/python.get.R
+++ R/python.get.R
@@ -2,11 +2,11 @@
 # CGB, 20100716, created
 #########################################################
 
-python.get <- function(var.name, string.code=FALSE){
+python.get <- function(var.name, string.code=FALSE, ...){
 
-    python.command <- paste0( "_r_return = object2json( [",
+    python.command <- paste0( "_r_return = object2json( ",
                              ifelse(string.code, var.name, deparse(substitute(var.name))),
-                             "] )" )
+                             " )" )
     python.exec( python.command, get.exception = TRUE, string.code=TRUE )
 
     ret <- .C( "py_get_var", "_r_return", not.found.var = integer(1), resultado = character(1), PACKAGE = "rPython" )
@@ -14,9 +14,6 @@ python.get <- function(var.name, string.code=FALSE){
     if( ret$not.found.var )
         stop( "Variable not found" )
         
-    ret <- fromJSON( ret$resultado )
-    if( length( ret ) == 1 ) ret <- ret[[1]]
-    if( class(ret) == "matrix" && dim(ret)[1] == 1) ret <- ret[1,]
-    ret
+    return(fromJSON(gsub('\\<nan?\\>','null',ret$resultado,ignore.case = T), ...))
 }
 
